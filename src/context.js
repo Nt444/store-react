590d00eb9f15a3840145029663c94a1b8ec22feb
Anatolii <quot.pro@gmail.com>
2019-02-04 12:18:26 +0300

    Up to 5.07 hour

--- src/context.js
+++ src/context.js
@@ -7,9 +7,12 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct: detailProduct,
-    cart: [],
+    cart: storeProducts,
     modalOpen: false,
     modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0,
   };
   componentDidMount() {
     this.setProducts();
@@ -65,6 +68,22 @@ class ProductProvider extends Component {
     }));
   }
 
+  increment = (id) => {
+    console.log('inc method');
+  }
+
+  decrement = (id) => {
+    console.log('dec method');
+  }
+
+  removeItem = (id) => {
+    console.log('rem method');
+  }
+
+  clearCart = (id) => {
+    console.log('clearCart method');
+  }
+
   render() {
     return (
       <ProductContext.Provider
@@ -73,7 +92,11 @@ class ProductProvider extends Component {
           handleDetail: this.handleDetail,
           addToCart: this.addToCart,
           openModal: this.openModal,
-          closeModal: this.closeModal
+          closeModal: this.closeModal,
+          increment: this.increment,
+          decrement: this.decrement,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart,
         }}
       >
         {this.props.children}
